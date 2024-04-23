1dc1f6c1df80cb7e64cebab57a3e0c3653063d90
GabrielPercincula <141972349+GabrielPercincula@users.noreply.github.com>
2024-04-23 12:21:33 -0300

    Update ListaDuplamenteLigada.cpp
    Alterado por Gabriel Percincula

--- ListaDuplamenteLigada/ListaDuplamenteLigada/ListaDuplamenteLigada.cpp
+++ ListaDuplamenteLigada/ListaDuplamenteLigada/ListaDuplamenteLigada.cpp
@@ -150,18 +150,70 @@ void inserirElemento()
 // funções a serem implementadas no exericio
 void exibirReverso()
 {
-
+	if (ultimo == NULL) {
+		cout << "Lista vazia \n";
+		return;
+	}
+	else {
+		cout << "Elementos: \n";
+		NO* aux = ultimo;
+		while (aux != NULL) {
+			cout << aux->valor << endl;
+			aux = aux->ant;
+		}
+	}
 }
 
 void excluirPrimeiroElemento()
 {
+	if (primeiro == NULL) 
+	{
+		cout << "lista vazia" << endl;
+		return;
+	}
+	else
+	{
+		NO* paraExcluir = primeiro;
+		primeiro = primeiro->prox;
+		
+		if (primeiro != NULL) {
+			primeiro->ant = NULL;
+		}
+		else {
+			ultimo = NULL;
+		}
+		free(paraExcluir);
 
+		cout << "primeiro elemento excluido" << endl;
+	}
 }
 
 void excluirUltimoElemento()
 {
+	if (ultimo == NULL)
+	{
+		cout << "lista vazia" << endl;
+		return;
+	}
+	else
+	{
+		NO* paraExcluir = ultimo;
+		ultimo = ultimo->ant;
 
+		if (ultimo != NULL) {
+			ultimo->prox = NULL;
+		}
+		else {
+			primeiro = NULL;
+		}
+
+		free(paraExcluir);
+
+		cout << "ultimo elemento excluido" << endl;
+
+	}
 }
+
 
 
 
